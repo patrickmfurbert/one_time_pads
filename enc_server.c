cf39cb2905b20272e9b91392b937552acd2e44d7
Patrick Furbert <patrickmfurbert@gmail.com>
2021-03-05 00:22:30 +0000

    filled in preliminary server code

--- enc_server.c
+++ enc_server.c
@@ -1,10 +1,126 @@
+/*
+    Patrick Furbert
+    CS344 - Operating Systems 1
+    March 4, 2021
+*/
+
+
+/*
+socket()
+bind()
+listen()
+accept()
+recv()
+send()
+close()
+*/
+
 
 
 //includes
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = inet_addr("127.0.0.1");
+  //address->sin_addr.s_addr = INADDR_ANY; 
+
+}
+
+
+/*
+    All errors after startup must only be printed to stderr - but the program must still run
+*/
 
 int main(int argc, char** argv){
+    
+    int connection_socket, characters_read, listen_socket;
+    char buffer[256];
+    struct sockaddr_in server_address, client_address;
+    socklen_t size_of_client_info = sizeof(client_address);
+
+
+    /*        Begin Startup             */
+    
+    if(argc < 1) { //check for incorrect number of arguments
+        fprintf(stderr, "USAGE: %s port\n", argv[0]);
+        exit(1);
+    }
+
+    setupAddressStruct(&server_address, atoi(argv[1])); //setup the address struct for the server socket
+
+    ////////////SOCKET/////////////////////
+    if((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        fprintf(stderr, "ERROR opening socket");
+        exit(1);
+    }
+
+    ////////////BIND///////////////////////
+    if(bind(listen_socket, 
+            (struct sockaddr *)&server_address,
+            sizeof(server_address)) < 0){
+        fprintf(stderr, "ERROR on binding");                                        
+    }
+
+    /*        End Startup                */
+
+    ////////////LISTEN/////////////////////
+    listen(listen_socket, 5);
+
+
+    /*          LOOP                     */
+    while(1){
+    ////////////ACCEPT/////////////////////
+        connection_socket = accept(listen_socket,
+                        (struct sockaddr *)&client_address,
+                        &size_of_client_info);
+        if(connection_socket < 0) {
+            fprintf(stderr, "ERROR on accept");
+            exit(1);
+        }
+
+        printf("SERVER: Connected to client running at host %d port %d\n",
+                                ntohs(client_address.sin_addr.s_addr),
+                                ntohs(client_address.sin_port));
+
+        //clear the buffer
+        memset(buffer, '\0', sizeof(buffer));
+
+    ////////////RECV///////////////////////
+        characters_read = recv(connection_socket, buffer, sizeof(buffer), 0);
+        if(characters_read < 0){
+            fprintf(stderr, "ERROR on reading from the socket");
+            exit(1);
+        }
+        printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+
+        ////////////SEND///////////////////////
+        characters_read = send(connection_socket, 
+                                "I am the server, and I got your message", 39, 0);
+        if(characters_read < 0){
+            fprintf(stderr, "ERROR on writing to socket");
+            exit(1);
+        }
+    ////////////CLOSE//////////////////////
+        close(connection_socket);
+    }
+
+    close(listen_socket); //close the listening socket
     return EXIT_SUCCESS;
 }
