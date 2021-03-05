2f339407c3f56d27532feb6ba77bbd5227d86bea
Patrick Furbert <patrickmfurbert@gmail.com>
2021-03-05 04:03:56 +0000

    working on enc_client

--- enc_client.c
+++ enc_client.c
@@ -1,10 +1,54 @@
+/*
+    Patrick Furbert
+    CS344 - Operating Systems 1
+    March 4, 2021
+*/
 
+/*
+    Client that sends the key and plain text to the encryption server
+*/
+
+
+/*
+socket()
+connect()
+send()
+recv()
+close()
+*/
 
 //includes
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>  // ssize_t
+#include <sys/socket.h> // send(),recv()
+#include <netdb.h>      // gethostbyname()
 
+// Set up the address struct for the server socket
+void setupAddressStruct(struct sockaddr_in* address, 
+                        int portNumber){
+ 
+  // Clear out the address struct
+  memset((char*) address, '\0', sizeof(*address)); 
+
+  // The address should be network capable
+  address->sin_family = AF_INET;
+  // Store the port number
+  address->sin_port = htons(portNumber);
+  // Allow a client at any address to connect to this server
+  address->sin_addr.s_addr = inet_addr("127.0.0.1");
+  //address->sin_addr.s_addr = INADDR_ANY; 
+
+}
 
 int main(int argc, char** argv){
+
+    if (argc<4){
+        fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); 
+        exit(0); 
+        }
+
     return EXIT_SUCCESS;
 }
--- enc_server.c
+++ enc_server.c
@@ -5,6 +5,10 @@
 */
 
 
+/* 
+    Encryption Server
+*/
+
 /*
 socket()
 bind()
